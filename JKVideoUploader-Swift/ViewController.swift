fdca4e7ab114a716228e4695a2f8b27ca8d19dce
jer-k <jeremy@carwoo.com>
2014-08-20 20:19:21 -0700

    Initial commit

---
+++ JKVideoUploader-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JKVideoUploader-Swift
+//
+//  Created by Jeremy Kreutzbender on 8/20/14.
+//  Copyright (c) 2014 Jeremy Kreutzbender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
